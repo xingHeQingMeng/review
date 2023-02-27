994b55a5b9fa4f7df3f271d8176c0e99eab4b49e
home <fjq18749395971@163.com>
2023-02-27 12:05:32 +0800

    docs: 常问代码实现

---
+++ 常问实现.js
@@ -0,0 +1,68 @@
+/**
+ * 数组去重
+ */
+const arr = [];
+
+// 1
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; ) {
+    if ((arr[i] = arr[j])) {
+      arr.splice(j, 1);
+    } else j++;
+  }
+}
+
+// 2
+const arr1 = [];
+for (var i = 0; i < arr.length; i++) {
+  if (arr1.indexOf(arr[i]) < 0) arr1.push(arr[i]);
+}
+// 3
+const newArr = [...new Set(arr)];
+
+/**
+ * 判断对象为空
+ */
+const obj = {};
+
+// 1
+JSON.stringify(obj) === '{}';
+
+// 2
+function isObjectEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+// 3
+Object.getOwnPropertyNames(obj).length === 0;
+
+// 4
+Object.keys(obj).length === 0;
+
+/**
+ * 复制数组
+ */
+
+// 1  循环逐一复制
+for (let i = 0; i < arr.length; i++) {
+  if (i in arr) newArr[i] = arr[i];
+}
+
+arr.map((_) => _);
+
+arr.reduce((v, a) => {
+  v.push(a);
+  return v;
+}, []);
+
+// 2 结构符
+const newarr = [...arr];
+
+// 3 slice 方法
+arr.slice();
+
+// 4 concat 方法
+arr.concat();
