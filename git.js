c2853e69b5f4e77fb6240620303695047bf7fd1b
home <fjq18749395971@163.com>
2023-02-27 12:03:14 +0800

    docs: git 常用指令

---
+++ git.js
@@ -0,0 +1,20 @@
+/**
+ * git 常用指令
+ *
+ * git branch 分支查看
+ * git branch branch_name 增加分支
+ * git checkout branch_name 切换分支
+ * git merge branch_name 合并 branch_name 分支到当前分支
+ * git branch -d branch_name 删除 branch_name 分支（已合并）
+ * git branch -D branch_name 删除 branch_name 分支（未合并）
+ * git remote 查看当前仓库管理的远程仓库信息
+ * git remote show origin 查看指定的远程仓库的详细信息
+ * git push --set-upstream origin branch_name 第一次将本地分支推到远程仓库
+ * git push 推送本地分支
+ * git pull 拉取远程分支到本地
+ * git push origin --delete branch_naame 删除远程分支
+ * git restore file_name 进行清除工作区的改变
+ * git tag 查看标签
+ * git tag v1.0.0 打标签
+ * git push origin v1.0.0 将tag同步到远程服务器
+ */
