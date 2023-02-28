71325968f65a829b37cf04ea6c495c4fc258c107
home <fjq18749395971@163.com>
2023-02-28 16:31:15 +0800

    docs: 闭包问题及优化

--- 闭包.js
+++ 闭包.js
@@ -50,3 +50,47 @@ function curryingCheck(reg) {
     return reg.test(txt);
   };
 }
+
+// 闭包的问题和优化
+/**
+ * 闭包：是指有权访问另一个函数作用域中的变量的函数
+ * 创建闭包的常见方式就是在函数内部创建另一个函数并返回该函数
+ *
+ * 作用：
+ *  1. 可以读取函数内部的变量
+ *  2. 相当于划出了一块私有作用域，避免数据污染
+ *  3. 让变量始终保存在内从中
+ *
+ * 闭包的三个特性：
+ *  1. 函数嵌套函数
+ *  2. 函数内部可以引用外部的参数和变量
+ *  3. 参数和变量不会被垃圾回收机制回收
+ *
+ * 闭包的问题：
+ *  闭包会产生不销毁的上下文，会导致栈/堆内存消耗过大，有时候也会导致内存泄漏等，
+ *  影响页面运行性能。
+ *
+ * 优化：不必要的情况下不在函数内部声明函数
+ */
+// 优化前
+function MyObject(name, message) {
+  this.name = name.toString();
+  this.message = message.toString();
+  this.getName = function () {
+    return this.name;
+  };
+  this.getMessage = function () {
+    return this.message;
+  };
+}
+// 优化后
+function MyObject(name, message) {
+  this.name = name.toString();
+  this.message = message.toString();
+}
+MyObject.prototype.getMessage = function () {
+  return this.message;
+};
+MyObject.prototype.getName = function () {
+  return this.name;
+};
