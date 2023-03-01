1a491eda006cdb33cd9ac3c2d3d968b76218adda
home <fjq18749395971@163.com>
2023-03-01 17:05:43 +0800

    docs: cookie 常见属性

--- cookie.js
+++ cookie.js
@@ -9,3 +9,13 @@
  * 
  * 路径一般设定为根目录 也就是 '/'
  */
+
+// cookie 的常见属性
+/**
+ * 键名 cookie键值对应的键名
+ * 键值 cookie键名对应的键值
+ * expires cooklie的失效，分为 session 会话时效，时间时效，时间时效是服务器时间，接就是世界标准时
+ * path 路径 符合路径的文件才能访问到cookie
+ * httponly 设置为 true 了之后，可以防止js程序访问，防止XSS攻击，增加cookie的安全性
+ * secure 设置为 true 之后 cookie 只能通过 https 协议发送，http协议是不能发送的，也是为了cookie的安全性
+ */
